42793ac521dc46eeccb66f9bd5019a61f1493149
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-07 11:43:47 +0530

    Orgin coomiit

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const app = express();
 const testResults = require('./test/result.json');
 const userData = require('./user/user.json');
 
+// Allow only specific origins
+const allowedOrigins = ['http://localhost:3000', 'https://next-load-dashboard.vercel.app'];
+
 app.use(cors({
-  origin: ['https://gyanaprakashkhandual.github.io', 'http://localhost:3000/', 'http://127.0.0.1:5500'],
-  credentials: true
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
+
+// Parse JSON bodies
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-} 
-);
+});
 
 app.get('/api/load-test-result', (req, res) => {
   res.json(testResults);
@@ -21,7 +32,7 @@ app.get('/api/load-test-result', (req, res) => {
 
 app.get('/api/user-data', (req, res) => {
   res.json(userData);
-})
+});
 
 app.listen(5000, () => {
   console.log('Server running on http://localhost:5000');
